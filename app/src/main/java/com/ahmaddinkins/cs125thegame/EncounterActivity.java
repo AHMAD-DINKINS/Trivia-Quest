323294f07395fc3ec7c1805f2bd4a2f0b4c531b9
kevin <kevin88tu@gmaillcom>
2018-12-02 15:43:34 -0600

    Added delay handler and fixed constraints

--- app/src/main/java/com/ahmaddinkins/cs125thegame/EncounterActivity.java
+++ app/src/main/java/com/ahmaddinkins/cs125thegame/EncounterActivity.java
@@ -1,14 +1,21 @@
 package com.ahmaddinkins.cs125thegame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.util.Log;
+
+import java.util.Timer;
 
 public class EncounterActivity extends AppCompatActivity {
+    private static final String TAG = "test";
+
     private RadioGroup radioAnswerGroup;
     private RadioButton radioAnswer;
     private Button answerButton;
@@ -21,6 +28,8 @@ public class EncounterActivity extends AppCompatActivity {
 
         result = (TextView) findViewById(R.id.result);
         addListenerButton();
+
+        Log.i(TAG, "onCreate");
     }
 
     public void addListenerButton() {
@@ -41,10 +50,65 @@ public class EncounterActivity extends AppCompatActivity {
 
                 if (radioAnswer.getText().equals("1")) {
                     result.setText("Correct");
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            startActivity(new Intent(EncounterActivity.this, MazeActivity.class));
+                        }
+                    }, 250);
                 } else {
                     result.setText("Incorrect");
                 }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
     }
 }
